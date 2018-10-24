8071ef4d2b6c0e276a9c8ff875ade0abf9e1ae7c
rickyyudhaa <43448299+rickyyudhaa@users.noreply.github.com>
2018-10-24 23:27:41 +0700

    Update PBO610117910Latihan51GajiKaryawan.java

--- PBO6-10117910-Latihan51-Gaji Karyawan/src/pbo6/pkg10117910/latihan51/gaji/karyawan/PBO610117910Latihan51GajiKaryawan.java
+++ PBO6-10117910-Latihan51-Gaji Karyawan/src/pbo6/pkg10117910/latihan51/gaji/karyawan/PBO610117910Latihan51GajiKaryawan.java
@@ -22,30 +22,29 @@ public class PBO610117910Latihan51GajiKaryawan {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Karyawan k = new Karyawan();
         Manager m = new Manager();
         Scanner scn = new Scanner(System.in);
         
         System.out.println("====Program Perhitungan Gaji Karyawan====");
         System.out.print("Masukan NIK : ");
-        k.setNik(scn.nextLine());
+        m.setNik(scn.nextLine());
         System.out.print("Masukan Nama : ");
-        k.setNama(scn.next());
+        m.setNama(scn.next());
         System.out.print("Masukan Golongan : ");
-        k.setGolongan(scn.nextInt());
+        m.setGolongan(scn.nextInt());
         System.out.print("Masukan Jabatan (Manager/Kabag): ");
-        k.setJabatan(scn.next());
+        m.setJabatan(scn.next());
         System.out.print("Masukan Jumlah Kehadiran : ");
         m.setKehadiran(scn.nextInt());
         
         System.out.println("====Hasil Perhitungan====");
-        System.out.println("NIK      :"+k.getNik());
-        System.out.println("NAMA     :"+k.getNama());
-        System.out.println("GOLONGAN : "+k.getGolongan());
-        System.out.println("JABATAN  :"+k.getJabatan());
+        System.out.println("NIK      :"+m.getNik());
+        System.out.println("NAMA     :"+m.getNama());
+        System.out.println("GOLONGAN : "+m.getGolongan());
+        System.out.println("JABATAN  :"+m.getJabatan());
         System.out.println("");
-        System.out.println("TUNJANGAN GOLONGAN      : "+m.tunjanganGolongan(k.getGolongan()));
-        System.out.println("TUNJANGAN JABATAN       : "+m.tunjanganJabatan(k.getJabatan()));
+        System.out.println("TUNJANGAN GOLONGAN      : "+m.tunjanganGolongan(m.getGolongan()));
+        System.out.println("TUNJANGAN JABATAN       : "+m.tunjanganJabatan(m.getJabatan()));
         System.out.println("TUNJANGAN KEHADIRAN     : "+m.tunjanganKehadiran(m.getKehadiran()));
         System.out.println("");
         System.out.println("GAJI TOTAL      : "+m.gajiTotal());
